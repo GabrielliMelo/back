23af232322287497b70b348e64a390d2622f2fe8
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-07-15 07:33:57 -0300

    chore: add express-async-errors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('express-async-errors');
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
@@ -8,5 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use((erro, req, res, next) => {
+  console.log(erro);
+  res.status(500);
+});
 
 module.exports = app;
