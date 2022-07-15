392f49cfaeca1e4c0adf7048160153704d0e346a
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-07-14 21:34:46 -0300

    chore: create route one pokemon

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ const pokemonController = require('../controller/PokemonController');
 const route = Router();
 
 route.get('/pokemons', pokemonController.getAllPokemons);
+route.get('/pokemons/:name', pokemonController.getPokemon);
 
 module.exports = route;
